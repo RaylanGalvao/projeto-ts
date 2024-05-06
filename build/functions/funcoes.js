561fddcd4b306499399d2428f8c10456c2cbae2d
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-05-06 11:53:48 -0300

    mais experiencia com funcoes em ts

--- build/functions/funcoes.js
+++ build/functions/funcoes.js
@@ -1,2 +1,20 @@
 "use strict";
-function login() { }
+function password(username) {
+    let msg = "bem vindo " + username;
+    console.log(msg);
+    return username;
+}
+const userNameLogin = password("raylan");
+console.log(userNameLogin);
+let n1 = 10;
+let n2 = 10;
+function soma(valor1, valor) {
+    let somaDosValores = valor1 + valor2;
+    if (somaDosValores > 40) {
+        return 'soma maior que 40';
+    }
+    else {
+        return 'soma menor que 40';
+    }
+}
+console.log(soma(n1, n2));
--- src/functions/funcoes.ts
+++ src/functions/funcoes.ts
@@ -1,2 +1,25 @@
 
-function login )
+function password (username: string): boolean | string{
+    let msg = "bem vindo " + username
+
+    console.log(msg)
+
+    return username
+}
+
+const userNameLogin = password("raylan")
+console.log(userNameLogin)
+
+let n1: number = 10
+let n2: number = 10
+
+function soma(valor1: number, valor:number): string{
+    let somaDosValores = valor1+valor2
+    if(somaDosValores>40){
+        return 'soma maior que 40'
+    }else {
+        return 'soma menor que 40'
+    }
+}
+
+console.log(soma(n1, n2))
